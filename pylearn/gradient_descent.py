73ccb586f17050703606659c51a92fd1ebdd8491
Tejas <tejasnarayan281@example.com>
2026-06-25 18:31:34 +0530

    made metrics.py and and make mae and mse , also made batch grad_descent

---
+++ pylearn/gradient_descent.py
@@ -0,0 +1,37 @@
+import numpy as np
+from .base import BaseEstimator
+
+class Batch_gradient_Descent(BaseEstimator):
+
+    def __init__(self,learning_rate,epochs):
+
+        self.weights = None
+        self.intercept = None
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+
+    def fit(self,X_train,y_train):
+
+        self.weights = np.ones(X_train.shape[1])
+        self.intercept = 0
+
+        X_train_arr = np.array(X_train)
+        y_train_arr = np.array(y_train)
+
+        n = len(y_train_arr)
+
+        for _ in range(self.epochs):
+
+            y_pred = np.dot(X_train_arr,self.weights) + self.intercept
+            loss = y_train_arr - y_pred
+
+            slope_weights = (-2 / n) * (X_train_arr.T @ loss)
+            slope_intercept = (-2 / n) * np.sum(loss)
+
+            self.weights -= self.learning_rate * slope_weights
+            self.intercept -= self.learning_rate * slope_intercept
+
+    def predict(self,X_test):
+        X_test_arr = np.array(X_test)
+        return np.dot(X_test_arr,self.weights) + self.intercept
+
--- pylearn/metrics.py
+++ pylearn/metrics.py
@@ -3,8 +3,18 @@ from .base import BaseEstimator
 
 class Regression_Metrics(BaseEstimator):
 
-    def __init__(self):
-        pass
+    def __init__(self,y_test,y_pred):
+        self.y_test = y_test
+        self.y_pred = y_pred
 
-    def MAE(y_test,y_pred):
+    def MAE(self):
         
+        mae = np.mean(np.abs(self.y_test - self.y_pred))
+        return mae
+    
+    def MSE(self):
+
+        mse = np.mean(self.y_test - self.y_pred)
+        return mse
+    
+    
